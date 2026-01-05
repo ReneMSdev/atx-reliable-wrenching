8186d5574e075bce8b8e603205a36ab8741ad4e4
renesalomone <108975981+renesalomone@users.noreply.github.com>
2026-01-05 13:41:55 -0600

    restructured files

---
+++ app/(site)/page.tsx
@@ -0,0 +1,13 @@
+import Nav from '@/components/layout/Nav'
+import Hero from '@/components/sections/hero/Hero'
+import Services from '@/components/sections/services/Services'
+
+export default function Home() {
+  return (
+    <>
+      <Nav />
+      <Hero />
+      <Services />
+    </>
+  )
+}
---
+++ app/components/sections/services/Services.tsx
@@ -0,0 +1,12 @@
+import { ServiceCard } from './ServiceCard'
+
+const Services = () => {
+  return (
+    <div>
+      <h1 className='text-4xl font-bold'>Our Services</h1>
+      <ServiceCard />
+    </div>
+  )
+}
+
+export default Services
