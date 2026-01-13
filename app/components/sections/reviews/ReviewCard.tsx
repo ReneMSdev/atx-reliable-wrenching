fef3217d917ddee3d04915f4fb1a5f2bae114b62
renesalomone <108975981+renesalomone@users.noreply.github.com>
2026-01-12 21:51:50 -0600

    decreased the size of the review cards

--- app/components/sections/reviews/ReviewCard.tsx
+++ app/components/sections/reviews/ReviewCard.tsx
@@ -20,7 +20,7 @@ function renderStars(rating: number): JSX.Element {
 
 export default function ReviewCard({ review, author, rating }: ReviewCardProps): JSX.Element {
   return (
-    <div className='relative w-full max-w-lg aspect-9/5'>
+    <div className='relative w-full max-w-md aspect-9/5'>
       <Image
         src='/images/review-frame.svg'
         alt='Review Frame'
@@ -29,14 +29,14 @@ export default function ReviewCard({ review, author, rating }: ReviewCardProps):
       />
 
       {/* Text overlay */}
-      <div className='absolute inset-0 pt-14 pb-8 px-8 flex flex-col justify-between'>
+      <div className='absolute inset-0 pt-10 md:pt-12 pb-8 px-8 flex flex-col justify-between'>
         {/* Review text (centered in remaining space) */}
         <div className='flex flex-1 items-start justify-center'>
-          <p className='text-md md:text-lg leading-snug italic line-clamp-6'>{review}</p>
+          <p className='text-md leading-snug italic line-clamp-4 md:line-clamp-6'>{review}</p>
         </div>
 
         {/* Bottom row */}
-        <div className='flex items-center justify-between text-md md:text-lg'>
+        <div className='flex items-center justify-between text-md'>
           <span className='font-bold'>{author}</span>
           {renderStars(rating)}
         </div>
