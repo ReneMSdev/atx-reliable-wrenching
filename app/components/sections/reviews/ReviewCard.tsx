05ac8050ef248907d64ead9c62af2ac89bece618
renesalomone <108975981+renesalomone@users.noreply.github.com>
2026-01-24 19:57:07 -0600

    line clamp 4 on small screens

--- app/components/sections/reviews/ReviewCard.tsx
+++ app/components/sections/reviews/ReviewCard.tsx
@@ -12,7 +12,7 @@ function renderStars(rating: number): JSX.Element {
   return (
     <div className='flex items-center text-[#FBBC04]'>
       {Array.from({ length: 5 }, (_, i) =>
-        i < rating ? <FaStar key={i} /> : <FaRegStar key={i} />
+        i < rating ? <FaStar key={i} /> : <FaRegStar key={i} />,
       )}
     </div>
   )
@@ -32,7 +32,7 @@ export default function ReviewCard({ review, author, rating }: ReviewCardProps):
       <div className='absolute inset-0 pt-10 md:pt-12 pb-8 px-8 flex flex-col justify-between'>
         {/* Review text (centered in remaining space) */}
         <div className='flex flex-1 items-start justify-center'>
-          <p className='text-md leading-snug italic line-clamp-3 md:line-clamp-6'>{review}</p>
+          <p className='text-md leading-snug italic line-clamp-4 md:line-clamp-6'>{review}</p>
         </div>
 
         {/* Bottom row */}
